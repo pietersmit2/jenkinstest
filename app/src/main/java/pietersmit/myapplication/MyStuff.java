0b9428e351a1ddea5824ee2fe1bce247a0e08a59
Pieter Smit <pieter@polymorph.co.za>
2016-01-20 14:10:15 +0200

    Text changed again!

---
+++ app/src/main/java/pietersmit/myapplication/MyStuff.java
@@ -0,0 +1,14 @@
+package pietersmit.myapplication;
+
+/**
+ * Created by pietersmit on 2016/01/20.
+ */
+public class MyStuff {
+
+    public int myAnswer(int number) {
+
+        return number * 2;
+
+    }
+
+}
